44fe319b5f2f2480d6d1850ff1301655e6b6ab2f
beataj <beata@jasinski.com>
2023-08-14 16:22:15 -0700

    fetch comments

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,8 +1,32 @@
 const loadCommentsBtnElement = document.getElementById('load-comments-btn');
+const commentsSectionElement = document.getElementById('comments');
 
-function fetchCommentsForPost() {
+function createCommentsList(comments) {
+    const commentListElement = document.createElement('ol');
+
+    for(const comment of comments) {
+        const commentElement = document.createElement('li');
+        commentElement.innerHTML = `
+            <article class="comment-item">
+            <h2>${comment.title}</h2>
+            <p>${comment.text}</p>
+            </article>
+        `;
+        commentListElement.appendChild(commentElement);
+    }
+
+    return commentListElement;
+}
+
+async function fetchCommentsForPost() {
     const postid = loadCommentsBtnElement.dataset.postid;
-    fetch(`/posts/${postid}/comments`);
+    const response =  await fetch(`/posts/${postid}/comments`);
+    const responseData = await response.json();
+    // console.log(responseData);
+
+    const commentsListElement =  createCommentsList(responseData);
+    commentsSectionElement.innerHTML = '';
+    commentsSectionElement.appendChild(commentsListElement);
 }
 
 loadCommentsBtnElement.addEventListener('click', fetchCommentsForPost);
